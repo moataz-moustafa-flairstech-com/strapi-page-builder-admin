50f798c85fb8337d98b569a31ebacddfcd688ffa
Moataz Moustafa <moataz.moustafa@flairstech.com>
2025-09-20 13:18:29 +0300

    add preview page support with flexible content blocks

--- config/admin.ts
+++ config/admin.ts
@@ -1,3 +1,18 @@
+
+const getPreviewPathname = (uid, { locale, document }) => {
+  const { slug } = document;
+  switch (uid) {
+    case "api::article.article": {
+      if (!slug) {
+        return "/articles"; // Article listing page
+      }
+      return `/articles/${slug}`; // Individual article page
+    }
+    default:
+      return null;
+  }
+};
+
 export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
@@ -16,5 +31,26 @@ export default ({ env }) => ({
   flags: {
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+  },
+  preview: {
+    enabled: true,
+    config: {
+      allowedOrigins: env("CLIENT_URL"),
+      async handler(uid, { documentId, locale, status }) {
+        const document = await strapi.documents(uid).findOne({ documentId });
+        const pathname = getPreviewPathname(uid, { locale, document });
+        if (!pathname) {
+          return null;
+        }
+        const previewSecret = env("PREVIEW_SECRET");
+        const clientUrl = env("CLIENT_URL");
+        const urlSearchParams = new URLSearchParams({
+          url: pathname,
+          secret: previewSecret,
+          status,
+        });
+        return `${clientUrl}/api/preview?${urlSearchParams}`;
+      },
+    },
   },
 });
